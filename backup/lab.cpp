be087ea487baf2f60bc6f0eaf30400a53caa492a
clumsy-sy <1725798537@qq.com>
2021-06-24 22:13:02 +0800

    6.24

--- backup/lab.cpp
+++ backup/lab.cpp
@@ -9,12 +9,10 @@
 using namespace std;
 typedef long long ll;
 
-#define _fora(i, a, n) for (ll i = (a); i <= (n); i++)
-#define _forz(i, a, n) for (ll i = (a); i >= (n); i--)
-#define _fore(i, a) for (int i = head[(a)]; i; i = edge[i].nxt)
+#define INF 0x3f3f3f3f
 #define LN putchar('\n')
 
-ll rr() {
+ll read() {
     ll s = 0, w = 1;
     char c = getchar();
     while (c < '0' || c > '9') {
@@ -32,29 +30,6 @@ ll rr() {
 #pragma endregion
 
 int main() {
-    ll n = rr(), m = rr();
-    _forz (k, n, 1) { // 倒数第 k 个
-        ll pos = -1;
-        _fora (i, k, n)
-            pos = (pos + m) % i;
-        if (k != 1)
-            printf("No%lld: %lld\n", n + 1 - k, pos + 1);
-        else
-            printf("Last No is: %lld\n", pos + 1);
-    }
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
-    printf("Hello \n");
+    
     return 0;
 }
